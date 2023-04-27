d132cf25ba04f21680f458ccbfd75537fb93f214
Tu Nombre <you@example.com>
2023-04-27 22:31:07 +0200

    4. Crud documental (elementos búsqueda en el listado documental) fix#6

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,76 +1,107 @@
 <script>
-
 import { mapState, mapActions } from 'pinia'
-// import axios from 'axios'
  import Documento from '@/views/Documento.vue'
  import { documentosStore } from '@/stores/documentos.js'
 
 export default {
    components: { Documento },
-//  components: { BasicLayout },
    data() {
-    return {
-    
-    }
+     return {
+        busquedaTitulo: '',
+        busquedaPorAutor: '',
+        buscaSinopsis: '',    
+      };
   },
  computed: {
-    ...mapState(documentosStore, ['documentos'])   
-
+    ...mapState(documentosStore, ['documentos']),    
+    filtroDeBusqueda() {
+  return this.documentos.filter((documento) =>
+    documento.titulo.toLowerCase().includes(this.busquedaTitulo.toLowerCase()) &&
+    documento.autor.toLowerCase().includes(this.busquedaPorAutor.toLowerCase()) &&
+    documento.sinopsis.toLowerCase().includes(this.buscaSinopsis.toLowerCase())
+  );
+},
   },
   methods: {
     //para get
-    ...mapActions(documentosStore, [ 'getDocumentos']),   
+    ...mapActions(documentosStore, [ 'getDocumentos' ]),   
   },    
   created() {
       this.getDocumentos()
   }
 };
-
 </script>
 
 <template>
-    <div class="container">
+      
        <img
         src="src/assets/logo_app_lite.png"
         class="img-fluid"
         alt="Descripción de la imagen"/>
-      <section class="contenedor-main row align-items-center">
-        <main class="col-md-8">     
-
           <h5 class="d-none d-md-block">
-            Encuentre el documento/libro o elemento audiovisual que necesita
+            Biblioteca del EMAD. Consulta del catálogo.
           </h5>
-          <br />
-          <p class="d-block d-md-none"></p>
-          <p>Búsqueda por autor</p>
-          <p>Búsqueda por título</p>
-          <br /><br />
-        </main>
+         <!-- <br/>
+           <h5>Consultar mis préstamos:  <button class="btn btn-lg btn-primary" type="button">Mis préstamos</button></h5>
+           -->
+         <br/> 
 
-        <aside class="col-md-4">
-          <h5>Consultar mis préstamos</h5>
-          <p class="d-none d-md-block">Mis préstamos</p>
-          <p class="d-block d-md-none"></p>
-        </aside>
-      </section>
+  <div class="row">
+      <div class="row">      
+  <div class="col-md-4 mb-3">
+  <h5>Buscar por Título</h5>
+    <input
+      v-model="busquedaTitulo"
+      type="text"
+      placeholder="Buscar por Título (introduzca el Título)"
+      class="form-control mb-3"/>
+  </div>
+  <div class="col-md-4 mb-3">
+   <h5>Buscar por Autor</h5>
+    <input
+      v-model="busquedaPorAutor"
+      type="text"
+      placeholder="Buscar por Autor (introduzca el Autor)"
+      class="form-control mb-3"/>
+  </div>
+  <div class="col-md-4 mb-3">
+   <h5>Buscar por Sinópsis</h5>
+    <input
+      v-model="buscaSinopsis"
+      type="text"
+      placeholder="Buscar por Sinopsis (introduzca palabras clave de la Sinopsis)"
+      class="form-control mb-3"
+    />
+  </div>
+</div>   
+  </div>
 
-      <section class="row">
-        <div class="col">
-          <h4>Documentos/Elementos audiovisuales disponibles</h4>   
-          <span>Esto lo pinta home</span>          
-      <div v-for="documento of documentos">{{ documento.autor }}</div>
-      <div v-for="documento of documentos">{{ documento }}</div>
-      
-      <!-- Componente documento -->
+  
+
+<div class="row">
+  <div class="col-md-12">
+    <ul>
+      <li v-for="documento in filtroDeBusqueda" :key="documento.id">
+        <strong>Título:</strong> {{ documento.titulo }}<br>
+        <strong>Autor:</strong> {{ documento.autor }}<br>
+        <strong>Sinopsis:</strong> {{ documento.sinopsis }}<br>
+        <strong>Estantería:</strong> {{ documento.estanteria }}<br>
+        <strong>Fecha Alta:</strong> {{ documento.fecha }}
+      </li>
+    </ul>
+  </div>
+</div>
+
       <!-- <Documento v-for="documento of documentos" :documento="documento">
       </Documento>  -->
-      </div>
-      </section>
-    </div>
+
 </template>
 
 <style scoped>
+
 .container {
-  width: 1800px;  
+  width: 1800px;
+  
 }
+
 </style>
