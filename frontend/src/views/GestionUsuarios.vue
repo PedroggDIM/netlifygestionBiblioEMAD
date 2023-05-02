6c2b60326e2ff6e38cf90f73a87c85639befc1a2
Tu Nombre <you@example.com>
2023-05-02 13:15:23 +0200

    9. Se añade un grupo de usuarios y se listan en pantalla Gestión de Usuarios fix#10

--- frontend/src/views/GestionUsuarios.vue
+++ frontend/src/views/GestionUsuarios.vue
@@ -1,17 +1,54 @@
 <script>
+import usuariosJson from '@/assets/usuarios.json'
 
 export default {
-  components: {
-   
+  components: {   
   },
+  data() {
+    return {
+      usuarios : usuariosJson.usuario
+    }
+  }
 
 };
-
 </script>
 
 
-<template>
-
-   <h2>Estamos de usuarios</h2>
- 
+<template> 
+   <!-- <div>
+     <div v-for="usuario of usuarios"> {{usuario.nombre}}  </div>
+   </div> -->
+<div class="container" id="user"> 
+  <h4>Usuarios de la aplicación</h4>
+  <div class="row">
+    <div class="col-md-12">
+      <table class="table table-striped table-hover table-bordered">
+        <thead>
+          <tr class="table-primary">
+            <th>UserName</th>
+            <th>Nombre</th>
+            <th>Apellidos</th>
+            <th>Extensión</th>
+            <th>Correo</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="usuario of usuarios">
+            <td>{{ usuario.username }}</td>
+            <td>{{ usuario.nombre }}</td>
+            <td>{{ usuario.apellidos }}</td>
+            <td>{{ usuario.telefono }}</td>
+            <td>{{ usuario.correo_electronico }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</div> 
 </template>
+
+<style>
+ #user { 
+     width: 500px;
+  }
+</style>
